4ca576185b07d179d2de41ba2227d6cc2aa06ff5
Jonathan Ngo <ngohuutrong.jonathan@gmail.com>
2023-01-04 23:09:48 +0700

    initial commit

---
+++ CacheRESTApiWithRedis/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CacheRESTApiWithRedis.Controllers;
+
+[Route("api/[controller]")]
+public class TestController : Controller
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hihi");
+    }
+}
